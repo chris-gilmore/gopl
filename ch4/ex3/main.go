737ca3e3d62e118c87fcbb5973f9190cf4ec3da3
Chris Gilmore <chris.gilmore@gmail.com>
2015-12-19 04:43:17 -0600

    Chapter 4: Exercise 3

---
+++ ch4/ex3/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const arraySize = 6
+
+func main() {
+	a := [arraySize]int{0, 1, 2, 3, 4, 5}
+	reverse(&a)
+	fmt.Println(a) // "[5 4 3 2 1 0]"
+}
+
+// reverse reverses a slice of ints in place.
+func reverse(a *[arraySize]int) {
+	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+}
